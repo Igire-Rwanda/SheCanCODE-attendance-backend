a185b2e20fa8984af6846e6ac594b4e16ed1bbf9
unknown <honorineuwituze896@gmail.com>
2022-11-17 16:10:52 +0200

    create superAdmin

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ import bodyParser from "body-parser";
 import cors from "cors"
 import dotenv from "dotenv";
 import route from "./src/Routes/index"
+import User from "./src/models/UserModels";
+import bcrypt, { hashSync } from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-dotenv.config ("./.env");
+
+dotenv.config ();
 const app=express();
 app.use (cors())
 
@@ -16,9 +20,30 @@ app.use("/v1/attendance",route);
 
 const dbUrl=process.env.DATABASEURL;
 
-mongoose.connect(dbUrl).then(()=> console.log("Database connected successfully"));
+const hashedPassword =bcrypt.hashSync(process.env.password,10);
+const createSuperAdmin = async()=>{
+    const admin =  new User({
+     email:process.env.EMAIL ,
+     password:hashedPassword,
+     role:"SuperAdmin"
+     
+  })
 
-const port=process.env.PORT ||4040;
+admin.save();
+ 
+ }
+    
+
+
+
+
+mongoose.connect(dbUrl).then(()=>{
+    console.log("Database connected successfully");
+    createSuperAdmin()
+});
+
+
+const port=process.env.PORT ||4000;
 
 app.listen(port,()=>{
     console.log(`Server is running on Port ${port}`);
