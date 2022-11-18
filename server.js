70def361e2b11f875887a51962ab84db5e2c0336
unknown <honorineuwituze896@gmail.com>
2022-11-18 14:50:23 +0200

    checking user

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ import cors from "cors"
 import dotenv from "dotenv";
 import route from "./src/Routes/index"
 import User from "./src/models/UserModels";
-import bcrypt, { hashSync } from 'bcrypt';
-
-
+import bcrypt from 'bcrypt';
 
 dotenv.config ();
 const app=express();
@@ -17,37 +15,28 @@ app.use(bodyParser.json());
 
 app.use("/v1/attendance",route);
 
-
 const dbUrl=process.env.DATABASEURL;
 
-const hashedPassword =bcrypt.hashSync(process.env.password,10);
+
 const createSuperAdmin = async()=>{
+
+  const findUser = await User.findOne({email:process.env.email});
+  if(!findUser){
     const admin =  new User({
      email:process.env.EMAIL ,
-     password:hashedPassword,
-     role:"SuperAdmin"
-     
+     password:bcrypt.hashSync(process.env.PASSWORD,10),
+     role:"SuperAdmin" 
   })
-//   userSchema.path('email').validate(async (email)=>{
-//     const emailCount = await mongoose.models.Users.countDocuments({ email })
-//     return !emailCount
-//   },'email used')
+  admin.save();
 
-  
+}
+}
 
-admin.save();
- 
- }
     
-
-
-
-
 mongoose.connect(dbUrl).then(()=>{
     console.log("Database connected successfully");
     createSuperAdmin()
 });
-
 
 const port=process.env.PORT ||4000;
 
