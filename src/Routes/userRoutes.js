d5ac841832e383a1bc0490e2a0dfeb18f85959d9
winnyh20 <winny.ihirwe@gmail.com>
2022-11-30 09:04:45 +0000

    correctionLogIn

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import UserController from "../controller/UserController";
+import { checkUser,loginUser} from "../middlewares/CheckUser"
 // import getUserProfile from "../controller/UserController";
 // import VerifyAccess from "../middlewares/verifyAccess";
 
@@ -8,10 +9,12 @@ import UserController from "../controller/UserController";
 
 const route = express.Router();
 
-route.post("/login",UserController.login);
-route.get("/login", UserController.login);
+// route.post("/login",UserController.login);
+// route.get("/login", UserController.login);
+route.post("/login",loginUser);
+
 route.post("/mentors", UserController.addMentor);
-route.post("/create",UserController.createUser);
+route.post("/create",checkUser,UserController.createUser);
 route.get("/oneUser/:id",UserController.getOneUser);
 route.get("/getAll",UserController.getAllUser);
 route.patch("/updateUser/:id",UserController.updateOneUserById);
